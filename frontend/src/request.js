c41921b5850282f8d3038415cfcff19279dfc8bf
Pournima Gaikwad <pournima2496@gmail.com>
2023-04-08 19:54:51 +0530

    fetched tmdb api and improved banner stying

---
+++ frontend/src/request.js
@@ -0,0 +1,16 @@
+// we will store api key later in {process.env.API_KEY}
+
+const API_KEY = "e69299439ffc0207b33e179aebb9034e";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networkks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests
