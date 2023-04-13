9401ee5a88fc04fe03ffee727299e12d9d6c3ee3
Pournima Gaikwad <pournima2496@gmail.com>
2023-04-13 19:27:24 +0530

    firebase integration

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,8 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDR8L25vkUNKnL_tOGwLUlyKjzemKDkrf4",
-    authDomain: "netflix-clone-41150.firebaseapp.com",
-    projectId: "netflix-clone-41150",
-    storageBucket: "netflix-clone-41150.appspot.com",
-    messagingSenderId: "913284317125",
-    appId: "1:913284317125:web:58d3336db4e7da95b115ac"
-  };
+  apiKey: "AIzaSyDR8L25vkUNKnL_tOGwLUlyKjzemKDkrf4",
+  authDomain: "netflix-clone-41150.firebaseapp.com",
+  projectId: "netflix-clone-41150",
+  storageBucket: "netflix-clone-41150.appspot.com",
+  messagingSenderId: "913284317125",
+  appId: "1:913284317125:web:58d3336db4e7da95b115ac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
-import "./signup.scss"
-
+import { useRef } from "react";
+import "./signup.scss";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 const SignUp = () => {
-  return (
-    <div className='signup'>
-    <form>
-      <h1 className='signup_form'>Sign In</h1>
-      <input type="email" placeholder='Email address' />
-      <input type="password" placeholder='password' />
-      <button type='submit'>Sign Up</button>
-    </form>
-    </div>
-  )
-}
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
-export default SignUp
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(
+      auth,
+      emailRef.current.value,
+      passwordRef.current.value
+    )
+      .then((authUser) => console.log(authUser))
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+    console.log("Hello");
+  };
+
+  return (
+    <div className="signup">
+      <form>
+        <h1 className="signup_form">Sign In</h1>
+        <input ref={emailRef} type="email" placeholder="Email address" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
+        <h4>
+          <span className="gray">New to Netflix?</span>
+          <span className="link" onClick={register}>
+            {" "}
+            Sign Up Now
+          </span>
+        </h4>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
